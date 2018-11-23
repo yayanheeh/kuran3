234e0d0f104345cbab057adbcf990164eef42cee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-23 12:55:08 +0000

    Deploy mazipan/quran-offline to github.com/mazipan/quran-offline.git:gh-pages

--- sw.js
+++ sw.js
@@ -2,20 +2,24 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/a6f113c6904d66a1c8ce.js",
-    "revision": "929a38f52f2d81897f778493345f0f94"
+    "url": "/_nuxt/1c6b81827687c7626aa8.js",
+    "revision": "03f7ccc05041fc45f61cf7f1c5d4e02d"
   },
   {
-    "url": "/_nuxt/c564c8bbfd7218d01d9e.js",
-    "revision": "38cdeea4e25a8bda105cd49c054727ae"
+    "url": "/_nuxt/2c96320503279f57380f.js",
+    "revision": "5eb4b5fade624480c5d39a3f631e112f"
   },
   {
-    "url": "/_nuxt/e0d279ad06642dfd8c40.js",
-    "revision": "8679220423bcabcc780d16d51c0297eb"
+    "url": "/_nuxt/64bbc612109e49d7b886.js",
+    "revision": "bb3871d8565fafe0b45230c6dc8dd30d"
   },
   {
-    "url": "/_nuxt/eb6e0ca5076201dc9387.js",
-    "revision": "a15932cb3dfacfe1df6ffb599612c45c"
+    "url": "/_nuxt/96866691b8c8cd18c9cb.js",
+    "revision": "e52a1e373de5419be189bdc4e95fc3ae"
+  },
+  {
+    "url": "/_nuxt/fdb86c01e6291a1ce5ec.js",
+    "revision": "25fd53e305c3276d091bafd77f7b8c41"
   }
 ], {
   "cacheId": "quran-offline",
