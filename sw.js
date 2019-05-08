6667e691f66386262ab0964fa0367c420d41643e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-08 06:05:35 +0000

    Deploy mazipan/quran-offline to github.com/mazipan/quran-offline.git:gh-pages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.0/workbox/workbox-sw.js')
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js')
 
 // --------------------------------------------------
 // Configure
@@ -157,5 +157,5 @@ workbox.precaching.precacheAndRoute([
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/_nuxt/(?!.*(__webpack_hmr|hot-update))'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/(?!.*(__webpack_hmr|hot-update))'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
